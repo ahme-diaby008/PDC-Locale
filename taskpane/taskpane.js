d579df9a63df45259f145801cfb100fae817ed5d
ahme-diaby008 <ahme.diaby1@kingfisher.com>
2026-03-18 11:36:35 +0100

    Update taskpane.js

--- taskpane/taskpane.js
+++ taskpane/taskpane.js
@@ -1,4 +1,4 @@
-document.getElementById("myButton").addEventListener("click", async () => {
+document.addEventListener("DOMContentLoaded", function() {
   await fetch("https://d5a9f7d36f6be56ba17119f83f77d6.a6.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8dac9218fabd4357aa5821373ba16569/triggers/manual/paths/invoke?api-version=1", {
     method: "POST",
     headers: {
