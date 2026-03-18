63235f5be8aa2ed6900ceec43cb167702a347d40
ahme-diaby008 <ahme.diaby1@kingfisher.com>
2026-03-18 12:06:26 +0100

    Update taskpane.js

--- taskpane/taskpane.js
+++ taskpane/taskpane.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function onClick() {
   try {
-    const res = await fetch("https://REPLACE-WITH-YOUR-FLOW-URL", {
+    const res = await fetch("https://d5a9f7d36f6be56ba17119f83f77d6.a6.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8dac9218fabd4357aa5821373ba16569/triggers/manual/paths/invoke?api-version=1", {
       method: "POST",
       mode: "cors",
       headers: { "Content-Type": "application/json" },
