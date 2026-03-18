782f5e18dc6c60a0a68214e7dcdf3cf918a76848
ahme-diaby008 <ahme.diaby1@kingfisher.com>
2026-03-18 11:15:45 +0100

    Update taskpane.js

--- taskpane/taskpane.js
+++ taskpane/taskpane.js
@@ -1,13 +1,16 @@
 document.getElementById("myButton").addEventListener("click", async () => {
-    await fetch("https://d5a9f7d36f6be56ba17119f83f77d6.a6.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8dac9218fabd4357aa5821373ba16569/triggers/manual/paths/invoke?api-version=1", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            event: "button_clicked",
-            user: "demo"
-        })
-    });
-    alert("Action envoyée au flux !");
+  await fetch("https://d5a9f7d36f6be56ba17119f83f77d6.a6.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8dac9218fabd4357aa5821373ba16569/triggers/manual/paths/invoke?api-version=1", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      event: "button_clicked",
+      page: "taskpane.html",
+      timestamp: new Date().toISOString()
+    })
+  });
+
+  alert("Flux déclenché !");
 });
+``
