55a532569c366f2696e7e23ca196083b8c7972e5
ahme-diaby008 <ahme.diaby1@kingfisher.com>
2026-03-18 11:06:58 +0100

    Update fetch URL for Power Automate trigger

--- taskpane/taskpane.js
+++ taskpane/taskpane.js
@@ -1,5 +1,5 @@
 document.getElementById("myButton").addEventListener("click", async () => {
-    await fetch("URL_DE_VOTRE_FLUX_POWER_AUTOMATE", {
+    await fetch("https://d5a9f7d36f6be56ba17119f83f77d6.a6.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/8dac9218fabd4357aa5821373ba16569/triggers/manual/paths/invoke?api-version=1", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
